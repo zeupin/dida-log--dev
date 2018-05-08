6f9a6477b48ba6ff41058351f9a2023f7ed2cd93
Macc Liu <maccliu@qq.com>
2018-05-08 12:23:54 +0800

    static $dir 日志存放目录

--- src/Dida/Log/Log.php
+++ src/Dida/Log/Log.php
@@ -19,16 +19,41 @@ class Log
      */
     const VERSION = '20180508';
 
+    /**
+     * 默认的日志目录
+     *
+     * @var string
+     */
+    public static $dir = null;
+
 
     /**
      * 记录一行日志
      *
      * @param string $file
-     * @param type $data
+     * @param any $data
+     *
+     * @return bool 成功返回true，失败返回false
      */
-    public static function write($file, $data)
+    public static function write($data, $file = null)
     {
+        if (!$file) {
+            // 如果没有设置log目录，直接退出
+            if (self::$dir === null || !file_exists(self::$dir) || !is_dir(self::$dir)) {
+                return false;
+            }
+
+            // 日志文件名
+            $date = date('Y-m-d');
+            $file = realpath(self::$dir) . DIRECTORY_SEPARATOR . "{$date}.log";
+        }
+
+        // 日志内容
         $time = date("H:i:s");
+        $data = var_export($data, true);
         $result = file_put_contents($file, "$time $data\n", FILE_APPEND + LOCK_EX);
+
+        // 返回
+        return ($result === false) ? false : true;
     }
 }
