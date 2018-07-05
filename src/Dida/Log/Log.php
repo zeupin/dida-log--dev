bccd4742596f5a9a4d996092b33f697715848087
Macc Liu <maccliu@qq.com>
2018-07-05 15:33:39 +0800

    write()改为可变参数，writeTo()指定要写入的log文件

--- src/Dida/Log/Log.php
+++ src/Dida/Log/Log.php
@@ -28,30 +28,82 @@ class Log
 
 
     /**
-     * 记录一行日志
-     *
-     * @param string $file
-     * @param any $data
+     * 记录一条日志
      *
      * @return bool 成功返回true，失败返回false
      */
-    public static function write($data, $file = null)
+    public static function write($var)
     {
-        if (!$file) {
-            // 如果没有设置log目录，直接退出
-            if (self::$dir === null || !file_exists(self::$dir) || !is_dir(self::$dir)) {
-                return false;
-            }
-
-            // 日志文件名
-            $date = date('Y-m-d');
-            $file = realpath(self::$dir) . DIRECTORY_SEPARATOR . "{$date}.log";
+        // 参数个数
+        $num_args = func_num_args();
+        if (!$num_args) {
+            return false;
         }
 
-        // 日志内容
+        // 如果没有设置log目录，直接退出
+        if (self::$dir === null || !file_exists(self::$dir) || !is_dir(self::$dir)) {
+            return false;
+        }
+
+        // 日志文件名
+        $date = date('Y-m-d');
+        $file = realpath(self::$dir) . DIRECTORY_SEPARATOR . "{$date}.log";
+
+        // 参数列表
+        $args = func_get_args();
+
+        // 待输出内容
+        $output = [];
         $time = date("H:i:s");
-        $data = var_export($data, true);
-        $result = file_put_contents($file, "$time $data\n", FILE_APPEND + LOCK_EX);
+        foreach ($args as $arg) {
+            $content = var_export($arg, true);
+            $output[] = "$time $content\n";
+        }
+
+        // 写文件
+        $result = file_put_contents($file, implode('', $output), FILE_APPEND + LOCK_EX);
+
+        // 返回
+        return ($result === false) ? false : true;
+    }
+
+
+    /**
+     * 将一个变量记录到$dir/$file中。
+     *
+     * @param string $file
+     * @param any $var
+     */
+    public static function writeTo($file, $var)
+    {
+        // 参数个数
+        $num_args = func_num_args();
+        if (!$num_args) {
+            return false;
+        }
+
+        // 如果没有设置log目录，直接退出
+        if (self::$dir === null || !file_exists(self::$dir) || !is_dir(self::$dir)) {
+            return false;
+        }
+
+        // 日志文件名
+        $target = realpath(self::$dir) . DIRECTORY_SEPARATOR . "$file";
+
+        // 参数列表
+        $args = func_get_args();
+
+        // 待输出内容
+        $output = [];
+        $time = date("H:i:s");
+        for ($i = 1; $i < $num_args; $i++) {
+            $arg = $args[$i];
+            $content = var_export($arg, true);
+            $output[] = "$time $content\n";
+        }
+
+        // 写文件
+        $result = file_put_contents($target, implode('', $output), FILE_APPEND + LOCK_EX);
 
         // 返回
         return ($result === false) ? false : true;
