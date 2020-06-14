c4849aa3ea01bc59616b9536a39f99f7b078079a
Liu Nian <maccliu@qq.com>
2020-06-14 19:00:47 +0800

    文件头更新

--- src/Dida/Log/FileLogger.php
+++ src/Dida/Log/FileLogger.php
@@ -3,7 +3,7 @@
  * Dida Framework  -- PHP轻量级快速开发框架
  * 版权所有 (c) 上海宙品信息科技有限公司
  *
- * 官网: <https://github.com/zeupin/dida>
+ * Github: <https://github.com/zeupin/dida>
  * Gitee: <https://gitee.com/zeupin/dida>
  */
 namespace Dida\Log;
